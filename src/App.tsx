4774dee8a0d6e8bf29a83ec7723350b73966b256
Lucas Neves <lucas54neves@gmail.com>
2023-09-29 03:37:05 -0300

    feat: add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from './components/Button';
+
 export const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button variant={'primary'} />
+      <Button variant={'secondary'} />
+      <Button variant={'danger'} />
+      <Button variant={'success'} />
+      <Button />
     </>
   );
 };
