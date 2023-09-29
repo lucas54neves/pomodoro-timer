689b94b3fda75d9a3785e436c87b358af4e8a695
Lucas Neves <lucas54neves@gmail.com>
2023-09-29 03:52:22 -0300

    feat: add themes

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -17,8 +17,14 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) =>
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme.primary};
+
+  /* ${(props) =>
     css`
       background-color: ${buttonVariants[props.variant]};
-    `}
+    `}; */
 `;
