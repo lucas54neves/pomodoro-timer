be3701a5504d73402bd77a3ee6e734b69a8b0503
Lucas Neves <lucas54neves@gmail.com>
2023-09-29 04:17:46 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { ThemeProvider } from 'styled-components';
 
 import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <Button variant={'primary'} />
       <Button variant={'secondary'} />
       <Button variant={'danger'} />
